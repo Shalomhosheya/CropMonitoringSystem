e8ff7deb02b2eb391db2d09b049f382bc95aafbe
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2024-10-27 17:40:04 +0530

    "build frontEnd"

---
+++ controller/loginController.js
@@ -0,0 +1,30 @@
+document.getElementById("signUPBtn").addEventListener('click',function () {
+event.preventDefault()
+    var username= document.getElementById('usernameText').value;
+    var email= document.getElementById('emailText').value;
+    var password= document.getElementById('passwordtext').value;
+
+    console.log(username, email, password);
+
+    $.ajax({
+       url:'',
+       type:"POST",
+        data:JSON.stringify({
+           username:username,
+           email:email,
+           password:password
+        }),
+
+        contentType:'application/json',
+        success:function (response){
+            console.log("success "+response);
+            alert("Account Successfully created")
+        },
+        error:function (xhr,status,error){
+            console.log(xhr);
+            console.log(status);
+            console.log(error);
+            alert("Account creation failed")
+        }
+    });
+})
